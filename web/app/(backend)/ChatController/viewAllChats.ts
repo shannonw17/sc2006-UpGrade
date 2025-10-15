0499e1a6787e8ea8323a2d0bb3a6c4003e9600d5
shannonw17 <NTUshannon1011@gmail.com>
2025-10-15 14:46:05 +0800

    edited chat function

--- web/app/(backend)/ChatController/viewAllChats.ts
+++ web/app/(backend)/ChatController/viewAllChats.ts
@@ -1,7 +1,6 @@
 "use server";
 import prisma from "@/lib/db";
 import { revalidatePath } from "next/cache";
-import { requireUser } from "@/lib/requireUser";
 
 // in chat page, can view all the chats the current user has with other users
 
@@ -15,4 +14,15 @@ export async function viewAllChats(userId: string) {
         },
         orderBy: {createdAt: "desc"} //display most recent chat at top
     });
+    const chatFriends = Array.from(
+        new Set(
+            chats.map(m=> (m.senderId === userId? m.receiverId : m.senderId))
+        )
+    ); //return ids of users that current user have chat with
+    const friends = await prisma.user.findMany({
+        where: {id: {in: chatFriends}},
+        select: {username: true},
+    });
+    revalidatePath("/chats");
+    return friends;
 }
