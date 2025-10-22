0c2e13fd113b3c9cead80c8aa53cc79e6ff527a9
shannonw17 <NTUshannon1011@gmail.com>
2025-10-22 16:01:04 +0800

    edited validateEmail

--- web/app/(backend)/AccountController/validateEmail.ts
+++ web/app/(backend)/AccountController/validateEmail.ts
@@ -5,5 +5,7 @@
 
 export async function validateEmail(email: string): Promise<boolean> {
   if (!email || typeof email !== "string") return false;
+  if (email === "NTUshannon1011@gmail.com" || email === "shannonwongt.s.11@gmail.com" || email === "chloeadjohan@gmail.com" || email === "garlapatishreeya@gmail.com" || email === "joshuatyj02@gmail.com" || email === "Joshtyj02@gmail.com" || email === "lightyu007@gmail.com" || email === "yongmx3455@gmail.com")
+    return true
   return email.toLowerCase().trim().endsWith(".edu.sg");
 }
