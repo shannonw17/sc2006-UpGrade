4bf055c008455ac205c568161b4394bfa4069e09
shannonw17 <NTUshannon1011@gmail.com>
2025-10-28 18:20:43 +0800

    edited prisma

---
+++ web/app/(backend)/ChatController/createChat.ts
@@ -0,0 +1,11 @@
+//start chat when send message only if chat doesnt exist yet
+//else redirect to existing chat
+
+//only create a chat when message is sent
+
+
+//? but right now, no such thing as "Chat" in database --> literally just get all the messages with the UserId from database
+
+
+//notifications for chat?
+//show red when have unread notifs
